3a6d6ed0db9b61dc6715bc4130e5946f75d2d297
Anthony Veschi <anthonyveschi@gmail.com>
2020-02-29 20:27:26 -0500

    guages to visualize how steering works

---
+++ Gemu/Assets/Scripts/InputSliderScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSliderScript : MonoBehaviour
+{
+    float x;
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        x *= 200;
+        transform.localPosition = new Vector3(x, 0f, 1f);
+    }
+}
